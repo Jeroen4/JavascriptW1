25ff776d6795198f64f5a41219c511ce1e8d03e5
JeroenFaasse <99045518@mydavinci.nl>
2018-09-04 12:03:44 +0200

    .

--- lab2.js
+++ lab2.js
@@ -7,7 +7,7 @@ const ALERT = alert('U dient de volgende 3 vragen te beantwoorden.');       //ge
 
 var firstname = prompt('Vul je voornaam in');                               //maakt een variabele aan en vraagt naar de voornaam
 var surname = prompt('Vul je achternaam in');                               //maakt een variabele aan en vraagt naar de achternaam
-var age = prompt('Vul je leeftijd in');                                     //maakr een variabele aan en vraagt naar de leeftijd
+var age = prompt('Vul je leeftijd in');                                     //maakt een variabele aan en vraagt naar de leeftijd
 
 document.write("<br>Je voornaam: " + firstname);                            //vraagt naar de voornaam en laat het zien
 document.write("<br>Je achternaam: " + surname);                            //vraagt naar de achternaam en laat het zien
