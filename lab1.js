daf330f1f9c0743826e7e8436590bddaf81de2aa
JeroenFaasse <99045518@mydavinci.nl>
2018-09-04 11:49:42 +0200

    Added LAB 2

--- lab1.js
+++ lab1.js
@@ -3,7 +3,7 @@ Jeroen Faasse
 Applicatie ontwikkeling niveau 4, Davinci College Gorinchem
 */
 
-alert('U dient de volgende 3 vragen te beantwoorden.');                  //geeft een alert aan//
+alert('U dient de volgende 3 vragen te beantwoorden.');                  //geeft een alert aan
 
 document.write("<br>Je voornaam: " + prompt('Vul je voornaam in'));      //vraagt naar de voornaam en laat het zien
 document.write("<br>Je achternaam: " + prompt('Vul je achternaam in'));     //vraagt naar de achternaam en laat het zien
