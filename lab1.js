fab9a9f87918c4a81a326ace785951b4faa62efa
JeroenFaasse <99045518@mydavinci.nl>
2018-09-04 11:25:16 +0200

    finished

--- lab1.js
+++ lab1.js
@@ -5,7 +5,6 @@ Applicatie ontwikkeling niveau 4, Davinci College Gorinchem
 
 alert('U dient de volgende 3 vragen te beantwoorden.');                  //geeft een alert aan//
 
-
 document.write("<br>Je voornaam: " + prompt('Vul je voornaam in'));      //vraagt naar de voornaam en laat het zien
 document.write("<br>Je achternaam: " + prompt('Vul je achternaam in'));     //vraagt naar de achternaam en laat het zien
 document.write("<br>Je leeftijd: " + prompt('Vul je leeftijd in'));      //vraagt naar de leeftijd en laat het zien
