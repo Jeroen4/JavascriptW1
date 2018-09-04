1a82612bcbd7947da937c0340034b88a1ca037fe
JeroenFaasse <99045518@mydavinci.nl>
2018-09-04 11:47:47 +0200

    .

--- lab1.js
+++ lab1.js
@@ -7,4 +7,4 @@ alert('U dient de volgende 3 vragen te beantwoorden.');                  //geeft
 
 document.write("<br>Je voornaam: " + prompt('Vul je voornaam in'));      //vraagt naar de voornaam en laat het zien
 document.write("<br>Je achternaam: " + prompt('Vul je achternaam in'));     //vraagt naar de achternaam en laat het zien
-document.write("<br>Je leeftijd: " + prompt('Vul je leeftijd in'));      //vraagt naar de leeftijd en laat het zien
+document.write("<br>Je leeftijd: " + prompt('Vul je leeftijd in'));      //vraagt naar de leeftijd en laat het zien 
