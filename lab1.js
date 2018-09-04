5d074f5c5d3478c2e90ec89cec9e5b9171fddc94
JeroenFaasse <99045518@mydavinci.nl>
2018-09-04 10:20:53 +0200

    added variables

--- lab1.js
+++ lab1.js
@@ -1,5 +1,12 @@
-alert('U dient de volgende 3 vragen te beantwoorden.');
+// Jeroen Faasse//
+//Applicatie ontwikkeling niveau 4, Davinci College Gorinchem//
 
-prompt('Vul je voornaam in');
-prompt('Vul je achternaam in');
-prompt('Vul je leeftijd in');
+alert('U dient de volgende 3 vragen te beantwoorden.');//geeft een alert aan//
+
+var firstname = prompt('Vul je voornaam in');
+var surname = prompt('Vul je achternaam in'); //maakt een variabele aan en stelt direct de vragen//
+var age = prompt('Vul je leeftijd in');
+
+document.write(firstname);
+document.write(surname);      //laat de antwoorden zien//
+document.write(age);
