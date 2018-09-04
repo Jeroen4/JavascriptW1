2eb1bd7cc4bb684567457c488cdbba43c0f87643
JeroenFaasse <99045518@mydavinci.nl>
2018-09-04 11:13:42 +0200

    added comments en breaktags

--- lab1.js
+++ lab1.js
@@ -1,11 +1,11 @@
-// Jeroen Faasse//
-//Applicatie ontwikkeling niveau 4, Davinci College Gorinchem//
+/* 
+Jeroen Faasse
+Applicatie ontwikkeling niveau 4, Davinci College Gorinchem
+*/
 
 alert('U dient de volgende 3 vragen te beantwoorden.');//geeft een alert aan//
 
 
-document.write(prompt('Vul je voornaam in'));
-document.write('<br/>');
-document.write(prompt('Vul je achternaam in'));
-document.write('<br/>');                                //stelt de vragen en laat de antwoorden zien//
-document.write(prompt('Vul je leeftijd in'));
+document.write("<br>Je voornaam: " + prompt('Vul je voornaam in'));      //vraagt naar de voornaam en laat het zien
+document.write("<br>Je achternaam: " + prompt('Vul je achternaam in'));     //vraagt naar de achternaam en laat het zien
+document.write("<br>Je leeftijd: " + prompt('Vul je leeftijd in'));      //vraagt naar de leeftijd en laat het zien
