f28a02ef56d95d6fd0ea8595a09980f5e73bcf3c
JeroenFaasse <99045518@mydavinci.nl>
2018-09-04 10:26:33 +0200

    added breaks between answers

--- lab1.js
+++ lab1.js
@@ -3,10 +3,9 @@
 
 alert('U dient de volgende 3 vragen te beantwoorden.');//geeft een alert aan//
 
-var firstname = prompt('Vul je voornaam in');
-var surname = prompt('Vul je achternaam in'); //maakt een variabele aan en stelt direct de vragen//
-var age = prompt('Vul je leeftijd in');
 
-document.write(firstname);
-document.write(surname);      //laat de antwoorden zien//
-document.write(age);
+document.write(prompt('Vul je voornaam in'));
+document.write('<br/>');
+document.write(prompt('Vul je achternaam in'));
+document.write('<br/>');                                //stelt de vragen en laat de antwoorden zien//
+document.write(prompt('Vul je leeftijd in'));
